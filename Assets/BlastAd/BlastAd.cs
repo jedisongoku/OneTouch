28d66818a39ebab713a811bf876d512344163956
kenmiller <ken.miller521@gmail.com>
2018-06-12 15:59:13 -0700

    Imported Google Play, Unity Ads, AppsFlyer and corresponding scripts

---
+++ Assets/BlastAd/BlastAd.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BlastAd : MonoBehaviour
+{
+#region Public Variables
+    //Singleton reference
+    public static BlastAd instance;
+    //The time for the ad to be displayed until the exit button pops up
+    public float displayTime = 3f;
+    //Reference to the exit button
+    public Transform exitButton;
+    //Reference to the panel containing the ad
+    public Transform adPanel;
+    //If the ad is rewarded
+    public bool isRewardedAd;
+    //Reference to the video player
+    public VideoPlayer videoPlayer;
+    //Audio sources to enable and disable when Blast ad is playing
+    public AudioSource[] nonVideoAudioSources;
+#endregion
+
+#region Private Variables
+    //The incrementing time counter
+    private float timeCounter;
+    //if the ad is being shown
+    private bool isShowingAd;
+#endregion
+
+
+   
+    
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        if (videoPlayer.targetCamera == null)
+            videoPlayer.targetCamera = Camera.main;
+        ShowBlastAd();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(isShowingAd)
+        {
+            timeCounter += Time.deltaTime;
+            if(timeCounter >= displayTime)
+            {
+                exitButton.gameObject.SetActive(true);
+                isShowingAd = false;
+            }
+        }
+        
+	}
+    //Show the Blast Ad
+    public void ShowBlastAd()
+    {
+        if (videoPlayer.targetCamera == null)
+            videoPlayer.targetCamera = Camera.main;
+        adPanel.gameObject.SetActive(true);
+        exitButton.gameObject.SetActive(false);
+        timeCounter = 0;
+        isShowingAd = true;
+        foreach (AudioSource source in nonVideoAudioSources)
+            source.enabled = false;
+        videoPlayer.Play();
+    }
+
+    public void HideBlastAd()
+    {
+        adPanel.gameObject.SetActive(false);
+        if(isRewardedAd)
+        {
+            AdReward();
+            isRewardedAd = false;
+        }
+        foreach (AudioSource source in nonVideoAudioSources)
+        {
+            source.enabled = true;
+            source.Play();
+        }            
+    }
+
+    public void OpenURL()
+    {
+        Application.OpenURL("https://app.adjust.com/m24ohje");
+    }
+
+    public void AdReward()
+    {
+        //Replace logic in this function to correctly reward the player
+    }
+}
---
+++ Assets/Energy Node/scripts/AppsFlyerMMP.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppsFlyerMMP : MonoBehaviour
+{
+
+    void Start()
+    {
+        // For detailed logging
+        //AppsFlyer.setIsDebug (true);
+        AppsFlyer.setAppsFlyerKey("aTYJZVwsYCTz8BbnbrDbxL");
+#if UNITY_IOS
+        //Mandatory - set your AppsFlyer’s Developer key.
+        
+        //Mandatory - set your apple app ID
+        //NOTE: You should enter the number only and not the "ID" prefix
+        AppsFlyer.setAppID ("YOUR_APP_ID_HERE");
+        AppsFlyer.trackAppLaunch ();
+#elif UNITY_ANDROID
+        //Mandatory - set your Android package name
+        AppsFlyer.setAppID("com.Blast.Collider.Test");
+        //Mandatory - set your AppsFlyer’s Developer key.
+        AppsFlyer.init("aTYJZVwsYCTz8BbnbrDbxL", "AppsFlyerTrackerCallbacks");
+
+        //For getting the conversion data in Android, you need to this listener.
+        //AppsFlyer.loadConversionData("AppsFlyerTrackerCallbacks");
+
+#endif
+    }
+
+    public static void LevelCompleted()
+    {
+
+        Dictionary<string, string> levelCompleted = new Dictionary<string, string>();
+        levelCompleted.Add("quantity", "1");
+        AppsFlyer.trackRichEvent("level_completed", levelCompleted);
+        Debug.Log("AppsFlyerMMP: Level Completed");
+    }
+
+    public static void InAppPurchase50Rubies()
+    {
+        Dictionary<string, string> purchaseEvent = new Dictionary<string, string>();
+        purchaseEvent.Add("af_currency", "USD");
+        purchaseEvent.Add("af_revenue", "0.99");
+        purchaseEvent.Add("af_quantity", "1");
+        AppsFlyer.trackRichEvent("iap_50rubies", purchaseEvent);
+        Debug.Log("AppsFlyerMMP: 50 Rubies IAP");
+    }
+}
